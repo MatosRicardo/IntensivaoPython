ec8e2eeb9dc8856aa1f4ea097aed17b0194b66b4
Ricardo Ribeiro <102837467+MatosRicardo@users.noreply.github.com>
2024-11-29 16:24:27 -0300

    Delete Basic directory

--- Basic/Exe2.py
+++
@@ -1,8 +0,0 @@
-#Exercício 2: Programa, carrinho de mercado
-
-item = input("Qual item você quer comprar? ")
-preco = float(input("Qual o preço do item? "))
-quantidade = int(input("Qual a quantidade do item? "))
-total = preco * quantidade
-
-print(f"Você comprou {quantidade} {item}(s) por um total de R${total:.2f}")
