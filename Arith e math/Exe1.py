f0cb53ccd76723f52f62efcf229e14d0b640ec39
Ricardo Ribeiro <102837467+MatosRicardo@users.noreply.github.com>
2024-11-29 16:24:35 -0300

    Delete Arith e math directory

--- Arith e math/Exe1.py
+++
@@ -1,9 +0,0 @@
-import math
-
-import math
-
-raio = float(input("Digite o raio do círculo: "))
-
-circunferencia = 2 * math.pi * raio
-
-print(f"A circunferência do círculo é {circunferencia:.2f}")
--- Arith e math/Exe3.py
+++
@@ -1,8 +0,0 @@
-import math
-
-a = float(input("Enter the value of a: "))
-b = float(input("Enter the value of b: "))
-
-c = (pow(a, 2) + pow(b,2))
-
-print(f"The value of c is {c:.2f}")
